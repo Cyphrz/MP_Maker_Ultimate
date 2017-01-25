16ae39e4eef420091dffa1f744a6012705f5299b
Robert Bodenhoefer <astroroxy@gmail.com>
2017-01-24 23:21:23 -0800

    Update Configuration.h
    Increase max temp to 275, will have dual firmware for stock and microswiss in the near future.
    Changed hotend PID to have more steady temps without overshoot.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -163,7 +163,7 @@
 // When temperature exceeds max temp, your heater will be switched off.
 // This feature exists to protect your hotend from overheating accidentally, but *NOT* from thermistor short/failure!
 // You should use MINTEMP for thermistor short/failure protection.
-#define HEATER_0_MAXTEMP 275
+#define HEATER_0_MAXTEMP 275 /LCD allows 285
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
 #define BED_MAXTEMP 130
@@ -199,9 +199,9 @@
     //#define  DEFAULT_Kd 114
 
 // Ultimaker2
-    #define  DEFAULT_Kp 9.12
-    #define  DEFAULT_Ki 0.41
-    #define  DEFAULT_Kd 50.98
+    #define  DEFAULT_Kp 62.16
+    #define  DEFAULT_Ki 9.21
+    #define  DEFAULT_Kd 104.91
 
 // Makergear
 //    #define  DEFAULT_Kp 7.0
